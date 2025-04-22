bc9f3eddd3d2513bdbf4ef5c12dff56908885fc8
Bougherara Adam <adam.bougherara@etu.univ-amu.fr>
2025-04-22 16:35:01 +0200

    Ajout du menubar

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,6 +1,12 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class FenetreLogiciel extends Application {
@@ -8,6 +14,31 @@ public class FenetreLogiciel extends Application {
     @Override
     public void start(Stage primaryStage) {
         // code de l'exercice 1
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 500, 500);
+
+        //Menu bar
+        Menu menu1 = new Menu("File");
+        MenuItem _new = new MenuItem("New");
+        MenuItem open = new MenuItem("Open");
+        MenuItem save = new MenuItem("Save");
+        MenuItem close = new MenuItem("Close");
+
+        menu1.getItems().addAll(_new, open, save, close);
+        Menu menu2 = new Menu("Edit");
+        MenuItem cut = new MenuItem("Cut");
+        MenuItem Copy = new MenuItem("Copy");
+        MenuItem paste = new MenuItem("Paste");
+        menu2.getItems().addAll(cut, Copy, paste);
+
+
+        Menu menu3 = new Menu("Help");
+
+        MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
+        root.setTop(menuBar);
+        primaryStage.setTitle("Premier exemple manipulant les conteneurs");
+        primaryStage.setScene(scene );
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
