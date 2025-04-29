82743214526704e76212c9cbc1041d18ace9dc78
Bougherara Adam <adam.bougherara@etu.univ-amu.fr>
2025-04-29 09:52:04 +0200

    Exo 4 terminé

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,6 +1,8 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -25,8 +27,48 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Création du label
+        label = new Label("Cliquez sur un bouton");
+        label.setAlignment(Pos.CENTER);
 
+        panneau = new Pane();
+        panneau.setStyle("-fx-background-color: white;");
+
+        vert = new Button("Vert");
+        rouge = new Button("Rouge");
+        bleu = new Button("Bleu");
+
+        vert.setOnAction(e -> {
+            nbVert++;
+            panneau.setStyle("-fx-background-color: green;");
+            label.setText("Vert cliqué " + nbVert + " fois");
+        });
+
+        rouge.setOnAction(e -> {
+            nbRouge++;
+            panneau.setStyle("-fx-background-color: red;");
+            label.setText("Rouge cliqué " + nbRouge + " fois");
+        });
+
+        bleu.setOnAction(e -> {
+            nbBleu++;
+            panneau.setStyle("-fx-background-color: blue;");
+            label.setText("Bleu cliqué " + nbBleu + " fois");
+        });
+
+        bas = new HBox(10, vert, rouge, bleu);
+        bas.setAlignment(Pos.CENTER);
+
+        root = new BorderPane();
+        root.setTop(label);
+        BorderPane.setAlignment(label, Pos.CENTER);
+        root.setCenter(panneau);
+        root.setBottom(bas);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setTitle("Palette de Couleurs");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
